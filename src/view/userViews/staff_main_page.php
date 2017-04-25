54069850946801ef06b5c7bf5b64a85b1f0edaf4
EkinK <ekinkandemir@gmail.com>
2017-04-26 00:13:54 +0300

    Add files via upload

---
+++ src/view/userViews/staff_main_page.php
@@ -0,0 +1,35 @@
+<html>
+
+	<meta charset="utf-8">
+	
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700|Lato:400,100,300,700,900' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Custom Stylesheet -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	
+</html>
+
+<body>
+
+	<?php 
+	 
+	include_once 'db_connect.php';
+
+	ob_start();
+	session_start();
+	 
+	if($_SESSION["login"] != "true"){
+		header("Location:index.php");
+	}
+	else {
+	
+	}
+	?>
+	<div class= "container"><div class="top">
+				<center><h1 id="title" class="hidden"><span id="logo"> <span>This is staff page!</span></span></h1>
+				<center><a href=index.php><p class="small">Log out</p></a>
+			</div></div>
+</body>
