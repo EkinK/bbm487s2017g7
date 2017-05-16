de029cd67ae4cd2bbfc2a0132a8a1f7431a1f846
EkinK <ekinkandemir@gmail.com>
2017-05-16 13:18:50 +0300

    Delete logout.php

--- src/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include_once 'functions.php';
-	mysqli_close($_SESSION["conn"]); /* close database connection */
-	session_destroy();	/* destroy session */
-	
-	header("Location:index.php");
-?>
